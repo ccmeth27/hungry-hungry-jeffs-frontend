2ceb7a7253609505029e00771d1458abd25dc8c5
VPoshelyuk <aslava198@gmail.com>
2019-11-12 14:52:39 -0500

    jeffs animation and stuff

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   
-  var context = document.querySelector("canvas").getContext("2d");
-  var balls = new Array();
-  let x = 300;
-  let y = 300;
-  for(let index = 0; index < 5; index ++) {
-    balls.push(new Ball(x, y, 15));
+  let context = document.querySelector("canvas").getContext("2d")
+  let balls = new Array()
+
+  let jeffImage = new Image()
+  jeffImage.src = "images/jeff-animation.png"
+  let jeff = new Sprite(context, 1010, 718, jeffImage, 90, 4)
+  let x = 300
+  let y = 300
+  for(let index = 0; index < 12; index ++) {
+    balls.push(new Ball(x, y, 15))
   }
   function loop() {
     let ballSprite = new Image()
@@ -16,26 +20,35 @@ document.addEventListener('DOMContentLoaded', () => {
     let ourJeffSprite = new Image()
     ourJeffSprite.src = "images/jeff.png"
     
-    window.requestAnimationFrame(loop);
-    let height = 600;
-    let width  = 600;
-    context.canvas.height = height;
-    context.canvas.width = width;
-    /* I removed that - 1 after making the video. It's not neccessary. */
+    window.requestAnimationFrame(loop)
+    let height = 600
+    let width  = 600
+    context.canvas.height = height
+    context.canvas.width = width
     for(let index = 0; index < balls.length; index ++) {
-      let ball = balls[index];
-      context.fillStyle = "rgba(0, 0, 0, 0)";
-      context.beginPath();
-      context.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
+      let ball = balls[index]
+      context.fillStyle = "rgba(0, 0, 0, 0)"
+      context.beginPath()
+      // context.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2)
       context.fill()
-      context.drawImage(ballSprite, ball.x, ball.y, 40, 40);
-      ball.updatePosition(width, height);
+      context.drawImage(ballSprite, ball.x, ball.y, 40, 40)
+      
+      if (ball.x < 270 && ball.x > 220 && ball.y > 500 && ball.y < 600) {
+        balls.splice(index, 1)
+      }
+
+      ball.updatePosition(width, height)
+
     }
-    context.beginPath();
-    // context.arc(300, 300, ball.radius, 0, Math.PI * 2);
-    context.fill()
-    context.drawImage(ourJeffSprite, 250, 550, 100, 100);
+    context.beginPath()
+    context.fillRect(255,420,90,80);
+    // context.arc(300, 300, ball.radius, 0, Math.PI * 2)
+    // context.fill()
+    // context.drawImage(ourJeffSprite, 250, 530, 100, 100)  
+    jeff.update()
+    jeff.render()
+
   }
-  loop();
+  loop()
   
 })
---
+++ src/sprite.js
@@ -0,0 +1,42 @@
+class Sprite{
+    constructor(context, width, height, image, ticksPerFrame, numberOfFrames){
+        this.context = context
+        this.width = width
+        this.height = height
+        this.x = 0
+        this.y = 0
+        this.image = image
+        this.scaleRatio = 0.4
+        this.frameIndex = 0
+        this.tickCount = 0
+        this.ticksPerFrame = ticksPerFrame 
+        this.numberOfFrames = numberOfFrames
+    }
+    update(){
+        this.tickCount += 1;
+        if (this.tickCount > this.ticksPerFrame) {
+            this.tickCount = 0;
+            // If the current frame index is in range
+            if (this.frameIndex < this.numberOfFrames - 1) {	
+                // Go to the next frame
+                this.frameIndex += 1;
+            } else {
+                this.frameIndex = 0
+            }
+        }
+    }
+
+    render(){
+        // Draw the animation
+        this.context.drawImage(
+        this.image,
+        this.frameIndex * this.width / this.numberOfFrames,
+        0,
+        this.width / this.numberOfFrames,
+        this.height,
+        250, 
+        330,
+        this.width / this.numberOfFrames * this.scaleRatio,
+        this.height * this.scaleRatio)
+    }
+}
