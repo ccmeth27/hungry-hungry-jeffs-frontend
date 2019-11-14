24db7574c4a7df57da54e07a2e62d686d055d16b
Connor Meth <connormeth27@gmail.com>
2019-11-14 15:18:23 -0500

    frontend updates

--- src/index.js
+++ src/index.js
@@ -27,22 +27,47 @@ document.addEventListener('DOMContentLoaded', () => {
   jeffImage.src = "images/jeff-animation.png"
   let bezosImage = new Image()
   bezosImage.src = "images/bezos-animation.png"
+  let goldblumImage = new Image()
+
   let lebowskiImage = new Image()
   lebowskiImage.src = "images/lebowski-animation.png"
   
   let jeff = new Sprite(context, 505, 718, jeffImage, 0, 2)
   let jeffBezos = new Sprite(context, 505, 718, bezosImage, 0, 2)
   let jeffLebowski = new Sprite(context, 505, 718, lebowskiImage, 0, 2)
+  let jeffGoldblum = new Sprite(context, 505, 718, goldblumImage, 0, 2)
 
+  // generic Jeff sprites
   let riceBallSprite = new Image()
   riceBallSprite.src = "images/riceball.png"
   let basketBallSprite = new Image()
   basketBallSprite.src = "images/basketball.png"
   let edamameBallSprite = new Image()
   edamameBallSprite.src = "images/edamame.png"
+
+  // goldblum sprites
+  let dinosaurSprite = new Image()
+  dinosaurSprite.src = "images/t-rex-dinosaur-head.png"
+  let alienSprite = new Image ()
+  alienSprite.src = "images/spaceship.png"
+  let pianoSprite = new Image ()
+  pianoSprite.src = "images/piano.png"
+  
+  // bridges sprites
+  let whiteRussianSprite = new Image()
+  whiteRussianSprite.src = "images/white-russian.png"
+  let bowlingSprite = new Image ()
+  bowlingSprite.src = "images/bowling.png"
+  let tronDiscSprite = new Image ()
+  tronDiscSprite.src = "images/tron-disc.png"
+
+  // bezos sprites
+  let bookSprite = new Image ()
+  bookSprite.src ="images/books.png"
+  let boxSprite = new Image ()
+  boxSprite.src = "images/amazon-box.png"
   let moneyBallSprite = new Image()
   moneyBallSprite.src = "images/moneyz.png"
-
 
 
 
@@ -136,41 +161,59 @@ document.addEventListener('DOMContentLoaded', () => {
       loop(timeBar, scoreBar, jeff, edamameBallSprite, balls)
     }else if(parseInt(levelBar.innerText.split(" ")[1]) === 3 && parseInt(scoreBar.innerText.split(" ")[1]) > 600){
       levelBar.innerText = "Level 4"
-      for(let index = 0; index < 12; index ++) {
-        balls.push(new Ball(300, 300, 12))
+      for(let index = 0; index < 10; index ++) {
+        balls.push(new Ball(300, 300, 6))
       }
-      loop(timeBar, scoreBar, jeffLebowski, moneyBallSprite, balls)
+      loop(timeBar, scoreBar, jeff, dinosaurSprite, balls)
     }else if(parseInt(levelBar.innerText.split(" ")[1]) === 4 && parseInt(scoreBar.innerText.split(" ")[1]) > 600){
       levelBar.innerText = "Level 5"
       for(let index = 0; index < 12; index ++) {
-        balls.push(new Ball(300, 300, 12))
+        balls.push(new Ball(300, 300, 7))
       }
-      loop(timeBar, scoreBar, jeffLebowski, moneyBallSprite, balls)
+      loop(timeBar, scoreBar, jeff, alienSprite, balls)
     }else if(parseInt(levelBar.innerText.split(" ")[1]) === 5 && parseInt(scoreBar.innerText.split(" ")[1]) > 600){
       levelBar.innerText = "Level 6"
       for(let index = 0; index < 12; index ++) {
-        balls.push(new Ball(300, 300, 12))
+        balls.push(new Ball(300, 300, 6))
       }
-      loop(timeBar, scoreBar, jeffLebowski, moneyBallSprite, balls)
+      loop(timeBar, scoreBar, jeff, pianoSprite, balls)
     }else if(parseInt(levelBar.innerText.split(" ")[1]) === 6 && parseInt(scoreBar.innerText.split(" ")[1]) > 600){
       levelBar.innerText = "Level 7"
       for(let index = 0; index < 12; index ++) {
-        balls.push(new Ball(300, 300, 12))
+        balls.push(new Ball(300, 300, 6))
       }
-      loop(timeBar, scoreBar, jeffLebowski, moneyBallSprite, balls)
+      loop(timeBar, scoreBar, jeffLebowski, whiteRussianSprite, balls)
     }else if(parseInt(levelBar.innerText.split(" ")[1]) === 7 && parseInt(scoreBar.innerText.split(" ")[1]) > 600){
       levelBar.innerText = "Level 8"
       for(let index = 0; index < 12; index ++) {
-        balls.push(new Ball(300, 300, 12))
+        balls.push(new Ball(300, 300, 7))
       }
-      loop(timeBar, scoreBar, jeffLebowski, moneyBallSprite, balls)
+      loop(timeBar, scoreBar, jeffLebowski, tronDiscSprite, balls)
     }else if(parseInt(levelBar.innerText.split(" ")[1]) === 8 && parseInt(scoreBar.innerText.split(" ")[1]) > 600){
       levelBar.innerText = "Level 9"
       for(let index = 0; index < 12; index ++) {
-        balls.push(new Ball(300, 300, 12))
+        balls.push(new Ball(300, 300, 9))
       }
-      loop(timeBar, scoreBar, jeffLebowski, moneyBallSprite, balls)
+      loop(timeBar, scoreBar, jeffLebowski, bowlingSprite, balls)
     }else if(parseInt(levelBar.innerText.split(" ")[1]) === 9 && parseInt(scoreBar.innerText.split(" ")[1]) > 600){
+      levelBar.innerText = "Level 10"
+      for(let index = 0; index < 12; index ++) {
+        balls.push(new Ball(300, 300, 6))
+      }
+      loop(timeBar, scoreBar, jeffBezos, bookSprite, balls)
+    }else if(parseInt(levelBar.innerText.split(" ")[1]) === 10 && parseInt(scoreBar.innerText.split(" ")[1]) > 600){
+      levelBar.innerText = "Level 11"
+      for(let index = 0; index < 12; index ++) {
+        balls.push(new Ball(300, 300, 10))
+      }
+      loop(timeBar, scoreBar, jeffBezos, boxSprite, balls)
+    }else if(parseInt(levelBar.innerText.split(" ")[1]) === 11 && parseInt(scoreBar.innerText.split(" ")[1]) > 600){
+      levelBar.innerText = "Level 12"
+      for(let index = 0; index < 12; index ++) {
+        balls.push(new Ball(300, 300, 8))
+      }
+      loop(timeBar, scoreBar, jeffBezos, moneyBallSprite, balls)
+    }else if(parseInt(levelBar.innerText.split(" ")[1]) === 12 && parseInt(scoreBar.innerText.split(" ")[1]) > 600){
       canvas.style.display = 'none'
       let img = document.createElement("img")
       img.src = "images/you-win.png"
